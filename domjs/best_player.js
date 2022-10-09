b958fb9081ba9f281e60c7fbf2b1ea4669d9bfd5
keviinsna <keevinsenna2001@gmail.com>
2022-10-09 18:20:18 -0300

    add feat hide players and caption of each player

--- domjs/best_player.js
+++ domjs/best_player.js
@@ -1,3 +1,6 @@
+// ---------------
+//      Data
+// ---------------
 const players = [
   {
     name: 'Lothar Matthäus',
@@ -20,93 +23,124 @@ const players = [
   {
     name: 'Romário',
     year: [1994],
-    img: '',
+    img: 'romario.jpg',
     quantity: 1,
   },
   {
     name: 'George Weah',
     year: [1995],
-    img: '',
+    img: 'george-weah.jpg',
     quantity: 1,
   },
   {
     name: 'Ronaldo',
     year: [1996, 1997, 2002],
-    img: '',
+    img: 'ronaldo.jpg',
     quantity: 3,
   },
   {
     name: 'Zinedine Zidane',
     year: [1998, 2000, 2003],
-    img: '',
+    img: 'zinedine-zidane.jpg',
     quantity: 3,
   },
   {
     name: 'Rivaldo',
     year: [1999],
-    img: '',
+    img: 'rivaldo.jpg',
     quantity: 1,
   },
   {
     name: 'Luís Figo',
     year: [2001],
-    img: '',
+    img: 'luis-figo.jpeg',
     quantity: 1,
   },
   {
     name: 'Ronaldinho',
     year: [2004, 2005],
-    img: '',
+    img: 'ronaldinho.jpg',
     quantity: 2,
   },
   {
     name: 'Fabio Cannavaro',
     year: [2006],
-    img: '',
+    img: 'fabio-cannavaro.jpg',
     quantity: 1,
   },
   {
     name: 'Kaká',
     year: [2007],
-    img: '',
+    img: 'kaka.jpg',
     quantity: 1,
   },
   {
     name: 'Cristiano Ronaldo',
     year: [2008, 2013, 2014, 2016, 2017],
-    img: '',
+    img: 'cristiano-ronaldo.jpg',
     quantity: 5,
   },
   {
     name: 'Lionel Messi',
     year: [2009, 2010, 2011, 2012, 2015, 2019],
-    img: '',
+    img: 'lionel-messi.jpg',
     quantity: 6,
   },
   {
     name: 'Luka Modrić',
     year: [2018],
-    img: '',
+    img: 'luka-modric.jpg',
     quantity: 1,
   },
   {
     name: 'Robert Lewandowski',
     year: [2020, 2021],
-    img: '',
+    img: 'robert-lewandowski.jpg',
     quantity: 2,
   },
 ];
+
+// ---------------
+//      Head
+// ---------------
+
+const icon = document.createElement('link');
+icon.setAttribute('rel', 'icon');
+icon.setAttribute('type', 'image/png');
+icon.setAttribute('href', './imgs/fifa-award-the-best.png');
+
+document.head.appendChild(icon);
+
+// ---------------
+//      Body
+// ---------------
+
+const h1 = document.createElement('h1');
+h1.append("The Best FIFA Men's Player");
+
+document.body.appendChild(h1);
 
 const buttonMinus = createButton('button-minus', '-');
 const counter = createCounter('counter');
 const buttonPlus = createButton('button-plus', '+');
 
-const divTrophies = body.appendChild(document.createElement('div'));
+const paragraph = document.createElement('p');
+paragraph.append(`Soccer player this quantity of fifa the best trophies:`);
+
+const divTrophies = document.body.appendChild(document.createElement('div'));
 divTrophies.setAttribute('id', 'trophies');
 
-const divPlayers = body.appendChild(document.createElement('div'));
-divPlayers.setAttribute('id', 'players');
+document.body.appendChild(paragraph);
 
+const divPlayers = document.body.appendChild(document.createElement('div'));
+divPlayers.setAttribute('id', 'players');
+divPlayers.style.display = 'flex';
+divPlayers.style.flexWrap = 'wrap';
+divPlayers.style.justifyContent = 'space-evenly';
+
+// ---------------
+//   Functions
+// ---------------
 
 function createButton(id, text) {
   const buttonElement = document.createElement('button');
@@ -139,21 +173,31 @@ function createImgTrophy(id) {
   return imgTrophyElement;
 }
 
-function createImgPlayer(player) {
-  const imgPlayer = document.createElement('img');
-  imgPlayer.setAttribute('src', `./imgs/${player.img}`);
-  imgPlayer.setAttribute('width', '150px');
-  imgPlayer.setAttribute('height', '150px');
-
-  imgPlayer.style.borderRadius = '10%';
-  imgPlayer.style.padding = '10px';
-
-  divPlayers.appendChild(imgPlayer);
-}
-
-function removeImgTrophy(id) {
+function removeImg(id) {
   const img = document.getElementById(id);
   if (img) img.remove(img);
+}
+
+function createImgPlayer(player) {
+  const figure = document.createElement('figure');
+  figure.setAttribute('id', player.name);
+
+  const figCaption = document.createElement('figCaption');
+  figCaption.append(player.name);
+  figCaption.style.textAlign = 'center';
+
+  const imgPlayer = document.createElement('img');
+  imgPlayer.setAttribute('src', `./imgs/${player.img}`);
+  imgPlayer.setAttribute('width', '300px');
+  imgPlayer.setAttribute('height', '300px');
+  imgPlayer.setAttribute('name', player.name);
+  imgPlayer.setAttribute('alt', player.name);
+  imgPlayer.style.borderRadius = '10%';
+
+  figure.appendChild(figCaption);
+  figure.appendChild(imgPlayer);
+
+  divPlayers.appendChild(figure);
 }
 
 buttonPlus.addEventListener('click', () => {
@@ -162,6 +206,7 @@ buttonPlus.addEventListener('click', () => {
     value = value + 1;
     counter.setAttribute('value', `${value}`);
     divTrophies.appendChild(createImgTrophy(value));
+    hidePlayers(value);
     showPlayers(value);
   }
 });
@@ -169,17 +214,26 @@ buttonPlus.addEventListener('click', () => {
 buttonMinus.addEventListener('click', () => {
   let value = parseInt(document.getElementById('counter').value);
   if (value > 0) {
-    removeImgTrophy(`trophy-${value}`);
+    removeImg(`trophy-${value}`);
     value = value - 1;
     counter.setAttribute('value', `${value}`);
+    hidePlayers(value);
     showPlayers(value);
   }
 });
 
 function showPlayers(quantityTrophies) {
   const playersFiltered = players.filter(
-    (player) => player.quantity == quantityTrophies
+    (player) => player.quantity === quantityTrophies
   );
 
   playersFiltered.map((player) => createImgPlayer(player));
+}
+
+function hidePlayers(quantityTrophies) {
+  const playersFiltered = players.filter(
+    (player) => player.quantity !== quantityTrophies
+  );
+
+  playersFiltered.map((player) => removeImg(player.name));
 }
